c192e23acfc4430ddaf2a33b4bf49e3cd469c8d4
Bob Lucassen <bat.lucassen@gmail.com>
2026-06-06 06:37:40 +0200

    exp29: uncapped holistics (prove for high confidence) + log holistic progress for monitoring

--- experiments/29_full_turn_search.py
+++ experiments/29_full_turn_search.py
@@ -225,7 +225,7 @@ for msolver in do_solve(mw_model, log=False, cores=CORES):
     t = time.time()
     hm, xv, cands, nc_ub, scoring = build_holistic(mword.lower(), turn_str)
     vbest = None
-    for vsolver in do_solve(hm, log=False, cores=CORES, time_limit=VCAP):
+    for vsolver in do_solve(hm, log=True, cores=CORES, time_limit=VCAP):   # log the holistic so we can watch the proof
         vbest = int(vsolver.objective_value)
         break
     dt = time.time() - t
